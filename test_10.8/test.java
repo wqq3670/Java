936f0e4dd6478481236da3a779daf8c26473b502
wqq3670 <1121093520@qq.com>
2019-10-13 19:25:16 +0800

    练习代码

---
+++ test_10.8/test.java
@@ -0,0 +1,27 @@
+public class test{
+	public static void main(String[] args){
+		
+		int num = 5;
+		int i = 1;
+		int ret = 1;
+		int sum = 0;
+		int j = 1;
+		while(i <= num) {
+			
+			ret = 1;
+			while(j <= i) {
+				
+				ret *= j;
+				j++;
+			
+			}
+			
+			sum += ret;
+			i++;
+		}
+		
+		System.out.println(sum);
+		
+	}
+}
+	
