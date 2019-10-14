9789f00fbb28d3238073c2410b45e770121aac52
wqq3670 <1121093520@qq.com>
2019-10-14 22:43:36 +0800

    更新练习

--- test_10.10/Test.java
+++ test_10.10/Test.java
@@ -2,49 +2,123 @@
 import java.util.Scanner;
 
 public class Test {
+	
+	//12. 获取一个数二进制序列中所有的偶数位和奇数位，分别输出二进制序列。 
+	public static void main(String[] args) {
+		
+		int [] Odd = new int[16];
+		int [] Eve = new int[16];
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入一个数：");
+		int n = sc.nextInt();
+		
+		//奇数位
+		for(int i = 1;i < 32;i+=2) {
+			int j = 0;
+			Odd[j] = ((n>>i)&1);
+			j++;
+		}
+		
+		//偶数位
+		for(int i = 0;i < 32;i+=2) {
+			int j = 0;
+			Eve[j] = ((n>>i)&1);
+			j++;			
+		}
+		
+		System.out.print("奇数数列为：");
+		for(int i = 15;i >= 0;i--) {
+			System.out.print(Odd[i]+" ");
+		}
+		System.out.println();
+		
+		System.out.print("偶数数列为: ");
+		for(int i = 15;i >= 0;i--) {
+			System.out.print(Eve[i]+" ");
+		}
+		sc.close();
+	}
+	
+	//11. 写一个函数返回参数二进制中 1 的个数 比如： 15 0000 1111 4 个 1
+	public static void main13(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("请输入一个数：");
+		int num = sc.nextInt();
+		int ret = funRet(num);
+		System.out.println("返回二进制中1的个数为："+ret);
+		//sc.close();
+	}
+	
+	public static int funRet(int n) {
+		
+		int count = 0;
+		for(int i = 0;i < 32;i++) {
+			
+			if(((n>>i)&1) == 1) {
+				
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	//10. 编写代码模拟三次密码输入的场景。 
+	//最多能输入三次密码，密码正确，提示“登录成功”,密码错误， 可以重新输 
+    //入，最多输入三次。三次均错，则提示退出程序 
+	public static void main12(String[] args) {
+		
+		String str1="Hello Java";
+		int i = 0;
+		for(i = 0;i < 3;i++) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("请输入密码：");
+			String str2 = sc.nextLine();
+			if(str1.equals(str2)==true) {
+				break;
+			}	
+		}
+		if(i < 3) {
+				System.out.println("登录成功");
+			}else {
+				System.out.println("输入密码错误，退出程序！");
+		}
+		//sc.close();		
+	}
+	
 	//9. 求出0～999之间的所有“水仙花数”并输出。
 	//(“水仙花数”是指一个三位数，其各位数字的立方和确好等于该数本 
     //身，如；153＝1＋5＋3?，则153是一个“水仙花数”。)
+	public static void main11(String[] args) {
 	
-	public static void main(String[] args) {
-	
-		//1.计算个数
-		int count = 1;
 		for(int i = 1;i <= 999;i++) {
 			
-			while(i/10 > 0) {
+			int count = 1;
+			int tmp = i;
+			double sum = 0.0;
+			
+			//1.计算个数
+			while(tmp/10 > 0) {
+				
 				count++;
-				i /= 10;
+				tmp /= 10;
+			}
+			tmp = i;
+			
+			//求和
+			while(tmp > 0) {
+				
+				sum += Math.pow(tmp%10,count);
+				tmp /= 10;
+			}
+			
+			//判断是否为水仙花数
+			if(i == sum) {
+				System.out.print(i+" ");
 			}
 		}
-		
-		//2.判断是否为水仙花数
-		
-		// int sum =0;
-		
-		// for(int i = 1;i <= 999;i++) {
-		
-			// if(i <= 9) {
-				
-				// System.out.println(i);
-			// }else {
-				// int tmp = i;
-				// while(tmp/10 != 0) {
-					
-					// sum += tmp%10;
-					// tmp /= 10;	
-				// }
-				
-				// if(i == sum) {
-					
-					// System.out.println(i);	
-				// }
-				
-			// }
-			
-		// }
-			
-		
 	}
 	
 	
