38c34c1964802a5117fce8a1bd1f4983b41cf0c4
wqq3670 <1121093520@qq.com>
2019-10-29 22:40:29 +0800

    练习代码

--- test_10.27/src/Test2.java
+++ test_10.27/src/Test2.java
@@ -1,43 +1,53 @@
+import java.util.Scanner;
+
 //编写一个类Calculator,有两个属性num1,num2,这两个数据的值，
 // 不能在定义的同时初始化，最后实现加减乘除四种运算.
 class Calculator{
     //属性
-    int num1;
-    int num2;
-
-    //构造函数(并对成员变量进行初始化)
-    public Calculator() {
-        this.num1 = 10;
-        this.num2 = 20;
-    }
+    private double num1;
+    private double num2;
 
     //方法
-
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
     //加
-    public int add (int x, int y) {
-        return x + y;
+    public double add () {
+        System.out.print("相加:");
+        return num1 + num2;
     }
-
     //减
-    public int sub(int x, int y) {
-        return x - y;
+    public double sub() {
+        System.out.print("相减:");
+        return num1 - num2;
     }
-
     //乘
-    public int mul(int x, int y) {
-        return x * y;
+    public double mul() {
+        System.out.print("相乘:");
+        return num1 * num2;
     }
-
     //除
-    public int div(int x, int y) {
-        return x / y;
+    public double div() {
+        System.out.print("相除:");
+        return num1 / num2;
     }
 }
 
 public class Test2 {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(5,3));
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入要操作的两个数：");
+        double x = sc.nextInt();
+        double y = sc.nextInt();
+        calculator.setNum1(x);
+        calculator.setNum2(y);
+        System.out.println(calculator.add());
+        System.out.println(calculator.sub());
+        System.out.println(calculator.div());
+        System.out.println(calculator.mul());
     }
 }
---
+++ test_10.29/src/TestDemo2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class TestDemo2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入要操作的两个数：");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        Swap swap1 = new Swap(x);
+        Swap swap2 = new Swap(y);
+        System.out.println("x = "+swap1.num);
+        System.out.println("y = "+swap2.num);
+        swap1.exchange(swap1,swap2);
+        System.out.println("x = "+swap1.num);
+        System.out.println("y = "+swap2.num);
+    }
+}
+
+class Swap {
+    //属性
+    public int num;
+
+//    public void setNum(int num) {
+//        this.num = num;
+//    }
+
+    //构造函数
+    public Swap(int num) {
+
+        this.num = num;
+    }
+    public void exchange(Swap swap1,Swap swap2) {
+        int c = 0;
+        Swap tmp = new Swap(c);
+
+        tmp = swap1;
+        swap1 = swap2;
+        swap2 = tmp;
+
+    }
+
+
+}
