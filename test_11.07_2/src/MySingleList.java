f3d59d5f847245b06e1ad176763d5269b05bd002
wqq3670 <1121093520@qq.com>
2019-11-08 22:26:32 +0800

    更新代码

--- test_11.07_2/src/MySingleList.java
+++ test_11.07_2/src/MySingleList.java
@@ -14,8 +14,8 @@ class ListNode {
     public int data;
     public ListNode next;
 
-    //构造函数进行初始化
     public ListNode(int data) {
+        this.data = data;
         this.next = null;
     }
 }
@@ -23,13 +23,10 @@ class MySingleList {
     //属性
     public ListNode head;
 
-    //构造方法进行初始化
     public MySingleList() {
         this.head  = null;
     }
-
     //方法
-
     //1.头插法
     public void addFirst(int data) {
         ListNode node = new ListNode(data);
@@ -57,8 +54,103 @@ class MySingleList {
         }
         cur.next = node;
     }
-    //3.
+    //3.任意位置插入,第一个数据节点为0号下标
+    private ListNode searchIndex(int index) {
+        ListNode prev = this.head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev.next;
+        }
+        return prev;
+    }
+    public boolean addIndex(int index,int data) {
+        ListNode node = new ListNode(data);
+        if (index < 0 || index > getlength()) {
+            return false;
+        }else {
+            if (index == 0) {
+                addFirst(data);
+                return true;
+            }
+            ListNode prev = searchIndex(index);
+            node.next = prev.next;
+            prev.next = node;
+            return true;
+        }
+    }
+    public int getlength() {
+        ListNode cur = this.head;
+        int count = 0;
+        while(cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
 
+    //4.打印单链表中的数据
+    public void display() {
+        if(this.head == null) {
+            return;
+        }
+        ListNode cur = this.head;
+        while(cur != null) {
+            System.out.print(cur.data+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    //5.查找是否包含关键字key是否在单链表当中
+    public boolean contains(int key) {
+        if (this.head == null) {
+            return false;
+        }
+        ListNode cur = this.head;
+        while(cur != null) {
+            if(cur.data == key) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    //6.删除第一次出现关键字为key的节点
+    public ListNode searchPrev(int key) {
+        ListNode prev = this.head;
+        while(prev.next != null) {
+            if(prev.next.data == key) {
+                return prev;
+            }
+            prev = prev.next;
+        }
+        return null;
+    }
+    public void remove(int key) {
+        if(this.head == null) {
+            return;
+        }
+        if(this.head.data == key) {
+            this.head = this.head.next;
+        }
+        //找前驱
+        ListNode prev = searchPrev(key);
+        if(prev == null) {
+            System.out.println("没有要删除的节点");
+            return;
+        }
+        ListNode del = prev.next;
+        prev.next = del.next;
+    }
+
+    //7.删除所有值为key的节点
+    public void removeAllKey(int key) {
+        if(this.head == null) {
+            return;
+        }
+        ListNode prev = this.head;
+        ListNode cur = this.head.next;
+    }
 
 
 
--- test_11.07_2/src/Test.java
+++ test_11.07_2/src/Test.java
@@ -7,6 +7,20 @@
  **/
 public class Test {
     public static void main(String[] args) {
-
+        MySingleList mySingleList = new MySingleList();
+        mySingleList.addFirst(1);
+        mySingleList.addFirst(2);
+        mySingleList.addFirst(3);
+        mySingleList.addFirst(4);
+        mySingleList.addFirst(5);
+        mySingleList.display();
+        mySingleList.addLast(6);
+        mySingleList.addLast(7);
+        mySingleList.display();
+        mySingleList.addIndex(0,0);
+        mySingleList.addIndex(8,8);
+        mySingleList.display();
+        mySingleList.remove(3);
+        mySingleList.display();
     }
 }
