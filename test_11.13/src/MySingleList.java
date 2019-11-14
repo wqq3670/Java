e6adf3fb26c5792ba619f0daf3f7cafc88281625
wqq3670 <1121093520@qq.com>
2019-11-14 20:33:08 +0800

    更新代码

--- test_11.13/src/MySingleList.java
+++ test_11.13/src/MySingleList.java
@@ -22,6 +22,71 @@ class MySingleList {
         this.head = null;
     }
 
+    //1.头插法
+    public void addFirst(int data) {
+        ListNode node = new ListNode(data);
+        if(this.head == null) {
+            this.head = node;
+        }else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+    //2.尾插法
+    public void addLast(int data) {
+        ListNode node = new ListNode(data);
+        if(this.head == null) {
+            this.head = node;
+        }else {
+            ListNode cur = this.head;
+            while(cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = node;
+        }
+    }
+    //3.任意位置插入一个data
+
+    /**
+     * 找到index -1的位置
+     * @param index
+     * @return
+     */
+
+    public ListNode searchIndex(int index) {
+        ListNode prev = this.head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev.next;
+        }
+        return prev;
+    }
+    /**
+     * 求链表长度
+     * @return
+     */
+    public int getlength() {
+        int count = 0;
+        ListNode cur = this.head;
+        while(cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+    public boolean addIndex(int index,int data) {
+        ListNode node = new ListNode(data);
+        if (index < 0 || index > getlength()) {
+            return false;
+        }
+        if(index == 0) {
+            addFirst(data);
+            return true;
+        }
+        ListNode prev = searchIndex(index);
+        node.next = prev.next;
+        prev.next = node;
+        return true;
+    }
 
 
 
--- test_11.13/src/Test1.java
+++ test_11.13/src/Test1.java
@@ -37,6 +37,47 @@ public class Test1 {
     }
 
     //输入两个链表，找出它们的第一个公共结点
+    public static ListNode getIntersectionNode
+                    (ListNode headA,ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+        ListNode pL = headA;//永远指向最长的链表
+        ListNode pS = headB;//永远指向最短的链表
+
+        //求各自链表长度 得出两个链表长度的差值
+        int lenA = 0;
+        int lenB = 0;
+        while(pL != null) {
+            lenA++;
+            pL = pL.next;
+        }
+        while(pS != null) {
+            lenB++;
+            pS = pS.next;
+        }
+        int len = lenA - lenB;
+        if(len < 0) {
+            pL = headB;
+            pS = headA;
+            len = lenB - lenA;
+        }else {
+            pL = headA;
+            pS = headB;
+        }
+
+        //让长的链表先走差值步
+        while(len > 0) {
+            pL = pL.next;
+            len--;
+        }
+        //一起一人一bu走
+        while(pL != pS) {
+            pL = pL.next;
+            pS = pS.next;
+        }
+        return pL;
+    }
 
 
     public static void main(String[] args) {
