b26334a28a2cb877c71170bfc5a0d4664f9dcb66
wqq3670 <1121093520@qq.com>
2019-11-27 23:24:28 +0800

    练习代码

---
+++ test_11.27/src/Test1.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2019-11-27
+ * Time: 23:19
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello java!");
+        System.out.println("I am wqq!");
+
+    }
+}
