3c05349b2a06b57e3759f5e6dc10e997a8dd94aa
wqq3670 <1121093520@qq.com>
2019-11-28 13:37:21 +0800

    提交代码

---
+++ test_11.28/src/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2019-11-28
+ * Time: 13:34
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
