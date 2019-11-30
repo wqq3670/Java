23176bee6d7aa8cfb2ce83e4c8e74bd6c564e19d
wqq3670 <1121093520@qq.com>
2019-11-30 23:10:00 +0800

    练习代码

---
+++ test_11.30/src/TestDome1.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2019-11-28
+ * Time: 13:35
+ **/
+public class TestDome1 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+    }
+}
