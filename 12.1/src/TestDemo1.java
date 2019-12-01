7700f286008614e5e3a3d28a48ea818550b593d7
wqq3670 <1121093520@qq.com>
2019-12-01 23:41:19 +0800

    练习代码

---
+++ 12.1/src/TestDemo1.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2019-12-01
+ * Time: 23:38
+ **/
+
+class TestDome1 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+    }
+}
+
