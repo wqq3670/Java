74865d8a0bca44bdc021b06fe1a6e130513c4486
wqq3670 <1121093520@qq.com>
2019-12-03 23:48:01 +0800

    更新代码

--- test_12.3/src/TestDemo1.java
+++ test_12.3/src/TestDemo1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -6,7 +8,23 @@
  * Time: 21:39
  **/
 public class TestDemo1 {
+    public static String func(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = chars.length - 1; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++)
+                if (chars[i] == chars[j]) {
+                    chars[i] = " ";
+                    break;
+                }
+        }
+        String tmp = new String(chars);
+        return tmp;
+    }
+
     public static void main(String[] args) {
-        System.out.println("冲冲冲");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String out = func(str);
+        System.out.println(out);
     }
 }
