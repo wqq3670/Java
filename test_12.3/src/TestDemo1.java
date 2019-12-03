5c77d2dd563d865450b49c3903bb093621933b0b
wqq3670 <1121093520@qq.com>
2019-12-03 21:41:30 +0800

    练习代码

---
+++ test_12.3/src/TestDemo1.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2019-12-03
+ * Time: 21:39
+ **/
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("冲冲冲");
+    }
+}
