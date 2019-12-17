d7dc40ed4fb39999bc27eead4dd6d57e450fd580
wqq3670 <1121093520@qq.com>
2019-12-17 21:46:21 +0800

    二叉树

--- test_12.15/src/BinaryTree.java
+++ test_12.15/src/BinaryTree.java
@@ -228,14 +228,32 @@ public class BinaryTree {
             return false;
         }
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-
     }
 
+    /**
+     *给定两个非空二叉树 s 和 t，
+     * 检验 s 中是否包含和 t 具有相同结构和节点值的子树。
+     * s 的一个子树包括 s 的一个节点和这个节点的所有子孙。
+     * s 也可以看做它自身的一棵子树。
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null || t == null) {
+            return false;
+        }
+        if(isSameTree(s,t)) {
+            return true;
+        }
+        if(isSubtree(s.left,t)) {
+            return true;
+        }
+        if(isSubtree(s.right,t)) {
+            return true;
+        }
+        return false;
 
-
-
-
-
-
+    }
 
 }
