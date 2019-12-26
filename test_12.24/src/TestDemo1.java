8cdef6fcf763f7e0cf6a238d8cb789836119dd10
wqq3670 <1121093520@qq.com>
2019-12-26 21:44:59 +0800

    快排

--- test_12.24/src/TestDemo1.java
+++ test_12.24/src/TestDemo1.java
@@ -45,7 +45,7 @@ public class TestDemo1 {
             array[j+1] = tmp;
         }
     }
-    public static void insertSort1(int[] array) {
+    public static void insertSort2(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int j = 0;
             int tmp = array[i];
@@ -77,6 +77,138 @@ public class TestDemo1 {
             }
         }
     }
+
+    /**
+     * 4.冒泡排序
+     * 时间复杂度：
+     * 最坏：O（n^2）
+     * 最好：O（n） 有序的  只走了一趟（下面代码已经优化）
+     * 空间复杂度：O（1）
+     * 稳定性：稳定的排序
+     * @param array
+     */
+    public static void bubbleSort(int[] array) {
+        boolean flag = false;
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1-i; j++) {
+                if(array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    flag = true;
+                }
+            }
+            if(!flag) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 5.快速排序
+     * 时间复杂度：O（n*log2n）
+     * 空间复杂度：O（log2n）因为有递归
+     * 稳定性：不稳定
+     * 快排比堆排快
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int partion(int[] array,int start,int end) {
+        int tmp = array[start];
+        while(start < end) {
+            while(start < end && array[end] >= tmp) {//注意"="号
+                end--;
+            }
+            if(start >= end) {
+                break;
+            }else {
+                array[start] = array[end];
+            }
+
+            while(start < end && array[start] <= tmp) {
+                start++;
+            }
+            if(start >= end) {
+                break;
+            }else {
+                array[end] = array[start];
+            }
+        }
+        //出了循环，end start相遇（指向了同一个位置）
+        array[start] = tmp;
+        return start;
+    }
+    public static void quick(int[] array,int low,int high) {
+        if(low >= high) {
+            return;
+        }
+        int pivot = partion(array,low,high);
+        //先左后右的递归
+        quick(array,low,pivot-1);
+        quick(array,pivot+1,high);
+    }
+    public static void quickSort(int[] array) {
+        quick(array,0,array.length-1);
+    }
+
+
+    /**
+     * 快排优化方式一 （在一定阈值里进行直接插入排序）
+     * 因为
+     */
+    //对于partion函数优化写法
+    public static int partion1(int[] array,int start,int end) {
+        int tmp = array[start];
+        while(start < end) {
+            while(start < end && array[end] >= tmp) {//注意"="号
+                end--;
+            }
+            array[start] = array[end];
+            while(start < end && array[start] <= tmp) {
+                start++;
+            }
+            array[end] = array[start];
+        }
+        //出了循环，end start相遇（指向了同一个位置）
+        array[start] = tmp;
+        return start;
+    }
+    public static void insertSort1(int[] array,int low,int high) {
+        for (int i = low+1; i <= high; i++) {
+            int j = 0;
+            int tmp = array[i];
+            for (j = i - 1; j >= 0; j--) {
+                if(array[j] > tmp) {
+                    array[j+1] = array[j];
+                }else {
+                    break;
+                }
+            }
+            array[j+1] = tmp;
+        }
+    }
+    public static void quick1(int[] array,int low,int high) {
+        if(low >= high) {
+            return;
+        }
+        if(high-low+1 < 100) {
+            insertSort1(array,low,high);
+            return;
+        }
+        int pivot = partion(array,low,high);
+        //先左后右的递归
+        quick(array,low,pivot-1);
+        quick(array,pivot+1,high);
+    }
+    public static void quickSort1(int[] array) {
+        quick(array,0,array.length-1);
+    }
+    /**
+     * 快排优化方式二 （三数取中法）
+     *
+     */
 
 
 }
