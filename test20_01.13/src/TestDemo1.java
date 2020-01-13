a92b6204ce7d64d91b764d18f4a6438c8e27e3a6
wqq3670 <1121093520@qq.com>
2020-01-13 23:53:59 +0800

    练习代码

---
+++ test20_01.13/src/TestDemo1.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-01-13
+ * Time: 16:15
+ **/
+public class TestDemo1 {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 5;
+        System.out.println(x+y+""+(x+y)+y);//25255
+    }
+
+}
