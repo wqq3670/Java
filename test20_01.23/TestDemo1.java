dad0445289b6da4e8cbb1713a672cba6a8fb3d88
wqq3670 <1121093520@qq.com>
2020-01-23 23:42:45 +0800

    练习代码

---
+++ test20_01.23/TestDemo1.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2019-12-12
+ * Time: 21:04
+ **/
+public class TestDemo1 {
+    public static void main(String[] args) {
+        Animal animal = new Cat("maomao","white",12);
+        animal.eat();
+    }
+}
