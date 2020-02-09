976a51d274462788d38e7f997573e997651f6c61
wqq3670 <1121093520@qq.com>
2020-02-09 20:41:30 +0800

    练习代码

---
+++ test20_02.09/src/TestDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-02-09
+ * Time: 14:34
+ **/
+public class TestDemo {
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，
+     * 请你在该数组中找出和为目标值的那两个整数，
+     * 并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。
+     * 但是，你不能重复利用这个数组中同样的元素。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
