8ddbbd6a506a5426efdfcde28da7836c4445dee5
wqq3670 <1121093520@qq.com>
2020-02-22 23:01:57 +0800

    练习代码

--- test20_02.21/src/TreeNode.java
+++ test20_02.21/src/TreeNode.java
@@ -33,5 +33,22 @@ public class TreeNode {
         return Math.abs(left - right) <= 1
                 && isBalanced(root.right) && isBalanced(root.right);
     }
+	public boolean isSymmetricChild(TreeNode leftTree,TreeNode rightTree) {
+        if(leftTree == null && rightTree == null) {
+            return true;
+        }
+        if(leftTree == null && rightTree != null || leftTree != null && rightTree == null) {
+            return false;
+        }
+        return leftTree.val == rightTree.val && isSymmetricChild(leftTree.left,rightTree.right) 
+        && isSymmetricChild(leftTree.right,rightTree.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        return isSymmetricChild(root.left,root.right);   
+    }
 }
 
