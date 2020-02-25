9c669723dbc9630f7abb837a8288975762c7a751
wqq3670 <1121093520@qq.com>
2020-02-25 23:00:44 +0800

    练习代码

--- test20_02.21/src/TreeNode.java
+++ test20_02.21/src/TreeNode.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -46,6 +50,151 @@ public class TreeNode {
         return Math.abs(left-right)<=1
                 && isBalanced(root.left)
                 && isBalanced(root.right);
+    }
+
+    /**
+     * 是否为对称二叉树
+     * @param root
+     * @return
+     */
+    private boolean isSymmetric(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        return isSymmetricChild(root.left,root.right);
+    }
+    private boolean isSymmetricChild(TreeNode leftroot,TreeNode rightroot) {
+        //两个子树都为空的情况
+        if(leftroot == null && rightroot == null) {
+            return true;
+        }
+        //两个子树只有一个为空的情况
+        if((leftroot==null&&rightroot!=null) || (leftroot!=null&&rightroot==null)) {
+            return false;
+        }
+        //对应值相等 并且 左树的左=右树的右  左树的右=右树的左
+        return leftroot.val == rightroot.val
+                && isSymmetricChild(leftroot.left,rightroot.right)
+                && isSymmetricChild(leftroot.right,rightroot.left);
+    }
+
+    /**
+     * 二叉树前序遍历（递归）
+     * @param root
+     * @return
+     */
+    private List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return null;
+        }
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return list;
+    }
+
+    /**
+     * 二叉树前序遍历（非递归）==》栈
+     * @param root
+     * @return
+     */
+    private List<Integer> preorderTraversalNor(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        //标志一个cur
+        TreeNode cur = root;
+        while(cur != null || !stack.empty()) {
+            while(cur != null) {
+                stack.push(cur);
+                list.add(cur.val);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            cur = cur.right;
+        }
+        return list;
+    }
+
+    /**
+     * 二叉树中序遍历（递归）
+     * @param root
+     * @return
+     */
+    private List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return null;
+        }
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        return list;
+    }
+
+    /**
+     * 二叉树中序遍历（非递归）==》栈
+     * @param root
+     * @return
+     */
+    private List<Integer> inorderTraversalNor(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.empty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+
+    /**
+     * 二叉树后序遍历（递归）
+     * @param root
+     * @return
+     */
+    private List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return null;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+    /**
+     * 二叉树后序遍历（非递归）==》栈
+     * @param root
+     * @return
+     */
+    private List<Integer> postorderTraversalNor(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        TreeNode prev = null;//代表刚刚弹出的节点
+        while(cur != null || !stack.empty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.peek();//cur等于栈顶元素但不弹出
+            if(cur.right == null || cur.right == prev) {
+                //cur.right为空 或者 cur.right 是刚刚已经弹出的元素
+                stack.pop();
+                list.add(cur.val);//弹出并记录
+                prev = cur;
+                cur = null;
+            }else {
+                cur = cur.right;
+            }
+        }
+        return list;
     }
 }
 
