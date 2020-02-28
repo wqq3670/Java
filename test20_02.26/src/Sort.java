090651ea29712d915da062982ad8e8b35785d21d
wqq3670 <1121093520@qq.com>
2020-02-28 22:18:41 +0800

    练习代码

--- test20_02.26/src/Sort.java
+++ test20_02.26/src/Sort.java
@@ -44,6 +44,23 @@ public class Sort {
             }
         }
     }
+    private static void bubbleSort1(int[] array) {
+        for (int i = 0; i < array.length-1; i++) {
+            boolean flag = false;
+            for (int j = 0; j < array.length-1-i; j++) {
+                if(array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    flag = true;
+                }
+            }
+            if(!flag) {
+                return;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] array = {1,8,92,88,66,55,41,94,12,4};
