729d0d16f0ee7224ebfa0ca7f3cc52a04d256296
wqq3670 <1121093520@qq.com>
2020-03-08 23:30:26 +0800

    练习代码

--- test20_03.06/src/Main1.java
+++ test20_03.06/src/Main1.java
@@ -27,15 +27,15 @@ public class Main1 {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
         String str2 = sc.nextLine();
-        System.out.println(deleteStr(str1,str2));
+       // System.out.println(deleteStr(str1,str2));
     }
 
-    private static String deleteStr(String str1, String str2) {
-        StringBuilder sb = new StringBuilder(str1);
-        for (int i = 0; i < str2.length(); i++) {
-            char ch = str2.charAt(i);
-
-        }
-
-    }
+//    private static String deleteStr(String str1, String str2) {
+//        StringBuilder sb = new StringBuilder(str1);
+//        for (int i = 0; i < str2.length(); i++) {
+//            char ch = str2.charAt(i);
+//
+//        }
+//
+//    }
 }
