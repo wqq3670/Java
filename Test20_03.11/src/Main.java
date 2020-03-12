883ee659eed98a8738383dd70b1c23721623e584
wqq3670 <1121093520@qq.com>
2020-03-12 23:26:20 +0800

    练习代码

--- Test20_03.11/src/Main.java
+++ Test20_03.11/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
      * n个数里最小的k个
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list = new ArrayList<>();
         while(sc.hasNext()) {
@@ -21,11 +21,31 @@ public class Main {
         int k = list.get(list.size()-1);
         list.remove(list.size()-1);
         Collections.sort(list);
-        for (int i = 0; i < k; i++) {
-            System.out.print(list.get(i));
+        for (int i = 0; i < k-1; i++) {
+            System.out.print(list.get(i)+" ");
         }
-
+        System.out.print(list.get(k-1));
     }
 
-
+    /**
+     * 分装函数写法
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        smallK(str);
+    }
+    private static void smallK(String str) {
+        String[] ss = str.split(" ");
+        int[] arr = new int[ss.length-1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(ss[i]);
+        }
+        int k = Integer.parseInt(ss[ss.length-1]);
+        Arrays.sort(arr);
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
--- Test20_03.11/src/Main2.java
+++ Test20_03.11/src/Main2.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
- * Description:
+ * Description:删数
  * User: dell
  * Date: 2020-03-11
  * Time: 22:15
@@ -10,14 +12,32 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i;
+        while(sc.hasNext()) {//处理循环输入
+            int n = sc.nextInt();
+            if(n > 1000) {//处理>1000的情况
+                n = 1000;
+            }
+            System.out.println(function(n));
         }
-        System.out.println(function(arr));
+    }
+    private static int function(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);//放入数据
+        }
+        int i = 0;
+        int ret = 0;
+        while(list.size() > 0) {
+            //得到要删除数现在所在的下标（随着删数size减少下标会变）
+            i = (i+2)%list.size();
+            if(list.size() == 1) {
+                //当size==1时，就是要得到的最后一个要删除的数
+                //它的值就是他原来所在的下标
+                ret = list.get(0);//此刻他一定在0号位置
+            }
+            list.remove(i);//删除数
+        }
+        return ret;
     }
 
-    private static int function(int[] arr) {
-    }
 }
